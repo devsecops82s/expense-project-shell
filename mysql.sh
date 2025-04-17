f13dcefdad56a61a312677a89b13a838864f34a0
srikanth <bommisettysrikanth88@gmail.com>
2025-04-17 19:30:56 +0530

    expense project started shell

---
+++ mysql.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script-logs"    # create dir in linux server --$ mkdir shell-script-logs--
+
+# only log file name logs
+LOG_FILE=$(echo $0 | cut -d "," -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDERS/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    dnf install mysql -y
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: you must have sudo access to execute this script" 
+        exit 1 
+    fi
+
+}
+
+echo " Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+    
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "starting MySQL Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAME
+VALIDATE $? "Setting Root Password"
+
+
+
+
+
